6ddc62d680624cf31ea6ed2a3f5cbf8cd3b1570f
duboisc43 <57966298+duboisc43@users.noreply.github.com>
2025-07-01 22:11:38 -0700

    Update test_simulator.py

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,1 +1,9 @@
+import pandas as pd
+from pipetting_sim.simulator import PipettingSimulator
 
+def test_optimize_transfers():
+    df = pd.DataFrame({'Sample': ['A'], 'Volume_uL': [250]})
+    sim = PipettingSimulator(df)
+    df2 = sim.optimize_transfers()
+    assert 'Transfers' in df2.columns
+    assert '2 transfer(s)' in df2['Transfers'][0]
